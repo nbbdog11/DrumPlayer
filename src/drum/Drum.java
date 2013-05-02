129221d8aa5ae20cd9d1d0c4768143c90b8a631b
Scott Wagner <jwagne12@villanova.edu>
2013-05-02 16:06:52 -0400

    cleaning up some resources

--- src/drum/Drum.java
+++ src/drum/Drum.java
@@ -30,6 +30,9 @@ public class Drum {
 			audio = AudioSystem.getAudioInputStream(file); //generate AudioInputStream from File object
 			Clip clip = AudioSystem.getClip(); //create audio Clip
 	        clip.open(audio); //open the AudioInputStream
+	        FloatControl gainControl = 
+	        	    (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+	        	gainControl.setValue(+5.0f); // Increase volume by 5 decibels.
 	        clip.start(); //play the sound clip
 		} catch (UnsupportedAudioFileException e) {
 			e.printStackTrace();
